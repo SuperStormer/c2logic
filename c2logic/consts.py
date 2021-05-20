7f4fc9f23fdbc7774a1339b155ae4557cfc842c8
Raphael Roberts <raphael.roberts48@gmail.com>
2021-05-20 00:14:28 -0500

    Sort SPECIAL_VARS

--- c2logic/consts.py
+++ c2logic/consts.py
@@ -34,4 +34,4 @@ func_binary_ops = ["pow", "max", "min", "angle", "len", "land", "idiv", "strictE
 func_unary_ops = ["abs", "log", "log10", "sin", "cos", "tan", "floor", "ceil", "sqrt", "rand"]
 binary_ops.update(dict(zip(func_binary_ops, func_binary_ops)))
 unary_ops.update(dict(zip(func_unary_ops, func_unary_ops)))
-SPECIAL_VARS = ("links", "ipt", "counter", "time", "unit", "tick")
+SPECIAL_VARS = ('counter', 'ipt', 'links', 'tick', 'time', 'unit')
