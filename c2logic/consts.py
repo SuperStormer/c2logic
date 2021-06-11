158963283ef058637dff10be6a2fa0a27d810106
Raphael Roberts <raphael.roberts48@gmail.com>
2021-06-11 11:13:40 -0500

    Added all special_vars

--- c2logic/consts.py
+++ c2logic/consts.py
@@ -34,4 +34,6 @@ func_binary_ops = ["pow", "max", "min", "angle", "len", "land", "idiv", "strictE
 func_unary_ops = ["abs", "log", "log10", "sin", "cos", "tan", "floor", "ceil", "sqrt", "rand"]
 binary_ops.update(dict(zip(func_binary_ops, func_binary_ops)))
 unary_ops.update(dict(zip(func_unary_ops, func_unary_ops)))
-SPECIAL_VARS = ('counter', 'ipt', 'links', 'tick', 'time', 'unit')
+SPECIAL_VARS = (
+	'counter', 'ipt', 'links', 'maph', 'mapw', 'this', 'thisx', 'thisy', 'tick', 'time', 'unit'
+)
